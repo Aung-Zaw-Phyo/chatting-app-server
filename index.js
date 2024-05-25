2c3ae1bc355b9c17927cfd412fa93d7c9a2be353
aungzawphyo <aungzawphyo1102@gmail.com>
2024-05-25 20:57:27 +0630

    refixing

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/chat').then(result => {
         console.log('Server is running on port: ', PORT)
     })
 
-    const io = require('./socket')(server, {
+    const io = require('./socket').init(server, {
         cors: corsOptions
     })
     io.on('connection', socket => {
